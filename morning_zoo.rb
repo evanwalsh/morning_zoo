d40db7463f93b12db64330979823235efcb1c75a
Evan Walsh <evan@nothingconcept.com>
2009-03-14 11:59:04 -0400

    Made it a bit more automatic

--- morning_zoo.rb
+++ morning_zoo.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/ruby
 
-military_ranks = ["Private","Corporal","Sergeant","Warrant Officer","Lieutenant","Captain","Major","Colonel","General"]
-xtreme_names = ["Fun","Awesome","Cool","Extreme","Rad","Boogie","Spike","Goofball","Dork"]
-animal_names = ["Cougar","Tiger","Bear","Lion","Mongoose","Cobra","Devil"]
-female_names = ["Sheila","Debby","Wanda","Debra","Betty","Sarah","Robin","Jess"]
-locations = ["Boneyard","Drop Zone","Jungle","Boogie House","Love Pad","Monkey Cage","Spooky Closet","Party Palace"]
+require "yaml"
 
-military_rank = military_ranks[rand(military_ranks.length)]
-xtreme_name = xtreme_names[rand(xtreme_names.length)]
-animal_name = animal_names[rand(animal_names.length)]
-female_name = female_names[rand(female_names.length)]
-location = locations[rand(locations.length)]
+data = YAML.load(File.read("data.yml"))
 
-puts military_rank+" "+xtreme_name+" and The "+animal_name+" with "+female_name+" in the "+location
+%w{military_ranks extreme_names animal_names female_names locations}.each do |list|
+  eval("@#{list} = data[\"#{list}\"]")
+end
+
+military_rank = @military_ranks[rand(@military_ranks.length)]
+extreme_name = @extreme_names[rand(@extreme_names.length)]
+animal_name = @animal_names[rand(@animal_names.length)]
+female_name = @female_names[rand(@female_names.length)]
+location = @locations[rand(@locations.length)]
+
+puts military_rank+" "+extreme_name+" and The "+animal_name+" with "+female_name+" in the "+location
