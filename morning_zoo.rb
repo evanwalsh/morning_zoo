0d0eb308a8ec8995193261f3c6055abcd435790b
Evan Walsh <evan@nothingconcept.com>
2009-03-14 12:12:05 -0400

    Less lines of code; Added plural/singular code

--- morning_zoo.rb
+++ morning_zoo.rb
@@ -1,17 +1,14 @@
 #!/usr/bin/ruby
 
 require "yaml"
+require "plural"
 
 data = YAML.load(File.read("data.yml"))
+@lists = %w{military_rank extreme_name animal_name female_name location}
 
-%w{military_ranks extreme_names animal_names female_names locations}.each do |list|
-  eval("@#{list} = data[\"#{list}\"]")
+@lists.each do |list|
+  eval("@#{list.plural} = data[ \"#{list.plural}\" ]")
+  eval("@#{list} = @#{list.plural}[ rand( @#{list.plural}.length ) ]")
 end
 
-military_rank = @military_ranks[rand(@military_ranks.length)]
-extreme_name = @extreme_names[rand(@extreme_names.length)]
-animal_name = @animal_names[rand(@animal_names.length)]
-female_name = @female_names[rand(@female_names.length)]
-location = @locations[rand(@locations.length)]
-
-puts military_rank+" "+extreme_name+" and The "+animal_name+" with "+female_name+" in the "+location
+puts @military_rank+" "+@extreme_name+" and The "+@animal_name+" with "+@female_name+" in the "+@location
